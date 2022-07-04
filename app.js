a3e5256699b050b58d5476d2564870e4c3d86e6d
Kadir Karaduman <qadirkaraduman@gmail.com>
2022-07-04 23:02:06 +0300

    next commit

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require ("./routes/courseRoute");
 
 const app = express();
 
+//Connect DB
+mongoose.connect('mongodb://localhost/smartedu-db')
+.then(() =>{
+  console.log("DB Connected Successfully")
+});
+
 //Template Engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 //Middlewares
-app.use(express.static("public"));
+app.use(express.static('public'));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 //Routes
-app.get('/', (req, res) => {
-  res.status(200).render("index", {
-    page_name: "index"
-  });
-});
-
-app.get('/about', (req, res) => {
-    res.status(200).render("about", {
-      page_name: "about"
-    });
-});
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
